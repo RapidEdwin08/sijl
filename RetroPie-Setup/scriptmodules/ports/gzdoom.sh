108536f343e23af23947d76e76704988ba856472
Rapid Edwin <88399538+RapidEdwin08@users.noreply.github.com>
2025-10-19 10:09:31 -0400

    Update gzdoom.sh

--- RetroPie-Setup/scriptmodules/ports/gzdoom.sh
+++ RetroPie-Setup/scriptmodules/ports/gzdoom.sh
@@ -26,6 +26,10 @@ function _get_version_gzdoom() {
     echo $gzdoom_version
 }
 
+function _get_version_zmusic_gzdoom() {
+    echo "1.3.0"
+}
+
 function depends_gzdoom() {
     local depends=(
         cmake libfluidsynth-dev libsdl2-dev libmpg123-dev libsndfile1-dev libbz2-dev
@@ -37,25 +41,31 @@ function depends_gzdoom() {
 
 function sources_gzdoom() {
     gitPullOrClone
-	# Apply Single-Board-Computer Specific Tweaks
-	if isPlatform "rpi"* || isPlatform "arm"; then
-		applyPatch "$md_data/00_sbc_tweaks.diff"
-	fi
-	# Apply SDL JoyPad Tweaks https://retropie.org.uk/forum/topic/16078/zdoom-and-gampad-fully-working-in-menu-with-no-keyboard
-	applyPatch "$md_data/01_sijl_tweaks.diff"
-	applyPatch "$md_data/02_JoyMappings_0SFA.diff"
-	applyPatch "$md_data/03_Preferences.diff" #ENABLED
+    # Apply Single-Board-Computer Specific Tweaks
+    if isPlatform "rpi"* || isPlatform "arm"; then
+        applyPatch "$md_data/00_sbc_tweaks.diff"
+    fi
+
+    # Apply SDL JoyPad Tweaks https://retropie.org.uk/forum/topic/16078/zdoom-and-gampad-fully-working-in-menu-with-no-keyboard
+    applyPatch "$md_data/01_sijl_tweaks.diff"
+    applyPatch "$md_data/02_JoyMappings_0SFA.diff"
+    applyPatch "$md_data/03_Preferences.diff" #ENABLED
+
     # add 'ZMusic' repo
     cd "$md_build"
+    ##gitPullOrClone zmusic https://github.com/ZDoom/ZMusic $(_get_version_zmusic_gzdoom)
     gitPullOrClone zmusic https://github.com/ZDoom/ZMusic
+
     # workaround for Ubuntu 20.04 older vpx/wepm dev libraries
     sed -i 's/IMPORTED_TARGET libw/IMPORTED_TARGET GLOBAL libw/' CMakeLists.txt
+
     # lzma assumes hardware crc support on arm which breaks when building on armv7
     isPlatform "armv7" && applyPatch "$md_data/lzma_armv7_crc.diff"
+
     # fix build with gcc 12 for armv8 on aarch64 kernel due to -ffast-math options
     if isPlatform "armv8"; then
-		if [[ "$__gcc_version" -eq 12 ]]; then applyPatch "$md_data/armv8_gcc12_fix.diff"; fi
-	fi
+        if [[ "$__gcc_version" -eq 12 ]]; then applyPatch "$md_data/armv8_gcc12_fix.diff"; fi
+    fi
 }
 
 function build_gzdoom() {
@@ -79,10 +89,11 @@ function build_gzdoom() {
 
 function install_gzdoom() {
     # 20251010 I'm tired of updating the libzmusic.so.1.* version...
-    local libzmusic_ver=libzmusic.so.1.3.0
+    local libzmusic_ver=libzmusic.so.$(_get_version_zmusic_gzdoom)
     if [[ ! -f "$md_build/release/zmusic/lib/$libzmusic_ver" ]]; then libzmusic_ver="$(basename $(ls $md_build/release/zmusic/lib/libzmusic.so.1.*))"; fi
     echo LIBZMUSIC.SO: [$libzmusic_ver]
-	md_ret_files=(
+
+    md_ret_files=(
         'release/brightmaps.pk3'
         'release/gzdoom'
         'release/gzdoom.pk3'
@@ -99,7 +110,7 @@ function install_gzdoom() {
 function add_games_gzdoom() {
     local params=("-fullscreen -config $romdir/ports/doom/gzdoom.ini -savedir $romdir/ports/doom/gzdoom-saves")
     local launcher_prefix="DOOMWADDIR=$romdir/ports/doom"
-	
+    
     # https://www.doomworld.com/forum/topic/99002-what-is-your-favorite-sector-light-mode-for-gzdoom/
     # 0 (Standard): Bright lighting model and stronger fading in bright sectors.
     # 1 (Bright): Bright lighting model and weaker fading in bright sectors.
@@ -134,8 +145,8 @@ function add_games_gzdoom() {
 
 function configure_gzdoom() {
     mkRomDir "ports/doom"
-	mkRomDir "ports/doom/mods"
-	mkRomDir "ports/doom/gzdoom-saves"
+    mkRomDir "ports/doom/mods"
+    mkRomDir "ports/doom/gzdoom-saves"
 
     moveConfigDir "$home/.config/$md_id" "$md_conf_root/doom"
 
@@ -144,4 +155,3 @@ function configure_gzdoom() {
     game_data_lr-prboom
     add_games_${md_id}
 }
-
